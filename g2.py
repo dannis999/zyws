ca2d005736770c11215212c9f7f54899bab4e055
dannis999 <39402843+dannis999@users.noreply.github.com>
2023-11-12 20:20:36 +0800

    1

--- g2.py
+++ g2.py
@@ -164,8 +164,7 @@ class worker:
         headers = get_headers()
         html = await self.get(host,headers=headers)
         t = re.escape(host)
-        t2 = re.escape('onclick="window.alert(&#39;为了防止刷票，请.+?验证&#39;)"')
-        pt = rf'a href\=\"({t}[a-z/]+/login\?id=)\d+\"\s+{t2}'
+        pt = rf'a href\=\"({t}[a-z/]+/login\?id=)\d+\"\s+onclick=\"window.alert([^)">]+?)\"'
         nexts = re.findall(pt,html)
         if not nexts:
             print(host)
